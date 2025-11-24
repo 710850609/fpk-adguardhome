41ca8badf945fffc02e829b15f915f2028fd2f94
BoB <710850609@qq.com>
2025-11-24 17:00:02 +0800

    更正脚本

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 fpk_version=0.1
-qinglong_version="latest"
-app_version="${fpk_version}-${qinglong_version}"
+adhome_version="latest"
+app_version="${fpk_version}-${adhome_version}"
 fpk_name="adguard-home-${app_version}.fpk"
 
 echo "开始打包 adguard-home.fpk"
